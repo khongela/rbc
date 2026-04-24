c95830d304a62b3f760bd7d03571c859a5d8dd5b
khongela <khongela7@gmail.com>
2026-04-24 21:29:56 +0200

    Part 1 complete and uploaded

---
+++ Part 1/display_board.py
@@ -0,0 +1,6 @@
+import chess
+
+board_state = input()
+board = chess.Board(board_state)
+
+print(board)
---
+++ Part 1/make_move.py
@@ -0,0 +1,10 @@
+import chess
+
+board_state = input()
+move = input()
+
+board = chess.Board(board_state)
+move = chess.Move.from_uci(move)
+board.push(move)
+
+print(board.fen())
