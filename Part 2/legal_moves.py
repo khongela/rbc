0106d30b0e3d4425fdb78c70e2d902d4d6909ac3
khongela <khongela7@gmail.com>
2026-04-25 09:03:03 +0200

    Part 2 submission 1 done

---
+++ Part 2/legal_moves.py
@@ -0,0 +1,18 @@
+import chess
+from reconchess.utilities import without_opponent_pieces, is_illegal_castle
+
+board_state = input()
+board = chess.Board(fen=board_state)
+
+moves = set()
+moves.add('0000')
+for move in board.pseudo_legal_moves:
+    moves.add(move.uci())
+    
+for move in without_opponent_pieces(board).generate_castling_moves():
+    if not is_illegal_castle(board, move):
+        moves.add(move.uci())
+
+print_moves = sorted(moves)
+for move in print_moves:
+    print(move)
